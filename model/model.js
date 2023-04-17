7445475e1d10cb7a95873cbab0d1c0ba5b2ba954
Kniswand <122379489+Kniswand@users.noreply.github.com>
2023-04-17 15:47:56 -0400

    Adding 'model' folder

---
+++ model/model.js
@@ -0,0 +1,87 @@
+var homeContent = `<div class="homeContent">
+<h1>Bank of Banks Home</h1>
+<img src="images/bankoflondon.jpg" />
+<p>A bank is a financial institution that accepts deposits from the public and creates a demand deposit while simultaneously making loans. Lending activities can be directly performed by the bank or indirectly through capital markets.</p>
+
+<p>Because banks play an important role in financial stability and the economy of a country, most jurisdictions exercise a high degree of regulation over banks. Most countries have institutionalized a system known as fractional-reserve banking, under which banks hold liquid assets equal to only a portion of their current liabilities. In addition to other regulations intended to ensure liquidity, banks are generally subject to minimum capital requirements based on an international set of capital standards, the Basel Accords.</p>
+<img src="images/bankofnepal.jpg" style="float:left"/>
+<p>Banking in its modern sense evolved in the fourteenth century in the prosperous cities of Renaissance Italy but in many ways functioned as a continuation of ideas and concepts of credit and lending that had their roots in the ancient world. In the history of banking, a number of banking dynasties, notably, the Medicis, the Fuggers, the Welsers, the Berenbergs, and the Rothschilds, have played a central role over many centuries. The oldest existing retail bank is Banca Monte dei Paschi di Siena (founded in 1472), while the oldest existing merchant bank is Berenberg Bank (founded in 1590).</p>
+<img src="images/nwb.jpg"/>
+<p>The word bank was taken into Middle English from Middle French <i>banque</i>, from Old Italian banco, meaning "table", from Old High German banc, bank "bench, counter". Benches were used as makeshift desks or exchange counters during the Renaissance by Florentine bankers, who used to make their transactions atop desks covered by green tablecloths.</p>
+
+<p>Activities undertaken by banks include personal banking, corporate banking, investment banking, private banking, transaction banking, insurance, consumer finance, trade finance and other related.</p>
+<br>
+</div>`;
+
+var historyContent = `<div class="historyContent">
+<h1>History of the Bank</h1>
+<h2>Ancient</h2>
+<img src="images/history.jpg"/>
+<p>The concept of banking may have begun in the times of ancient Assyria and Babylonia with merchants offering loans of grain as collateral within a barter system. Lenders in ancient Greece and during the Roman Empire added two important innovations: they accepted deposits and changed money Archaeology from this period in Iran, ancient China and India also shows evidence of money lending.</p>
+<h2>Medieval</h2>
+<img src="images/medieval.jpg" style="float:left"/>
+<p>The present era of banking can be traced to medieval and early Renaissance Italy, to the rich cities in the centre and north like Florence, Lucca, Siena, Venice and Genoa. The Bardi and Peruzzi families dominated banking in 14th-century Florence, establishing branches in many other parts of Europe. Giovanni di Bicci de' Medici set up one of the most famous Italian banks, the Medici Bank, in 1397. The Republic of Genoa founded the earliest-known state deposit bank, Banco di San Giorgio (Bank of St. George), in 1407 at Genoa, Italy.</p>
+<br>
+<h2>Early Modern</h2>
+<img src="images/modern.jpg"/>
+<p>Fractional reserve banking and the issue of banknotes emerged in the 17th and 18th centuries. Merchants started to store their gold with the goldsmiths of London, who possessed private vaults, and who charged a fee for that service. In exchange for each deposit of precious metal, the goldsmiths issued receipts certifying the quantity and purity of the metal they held as a bailee; these receipts could not be assigned, only the original depositor could collect the stored goods.</p>
+
+<p>Gradually the goldsmiths began to lend money out on behalf of the depositor, and promissory notes (which evolved into banknotes) were issued for money deposited as a loan to the goldsmith. Thus by the 19th century we find in ordinary cases of deposits of money with banking corporations, or bankers, the transaction amounts to a mere loan or mutuum, and the bank is to restore, not the same money, but an equivalent sum, whenever it is demanded and money, when paid into a bank, ceases altogether to be the money of the principal (see Parker v. Marchant, 1 Phillips 360); it is then the money of the banker, who is bound to return an equivalent by paying a similar sum to that deposited with him when he is asked for it. The goldsmith paid interest on deposits. Since the promissory notes were payable on demand, and the advances (loans) to the goldsmith's customers were repayable over a longer time-period, this was an early form of fractional reserve banking. The promissory notes developed into an assignable instrument which could circulate as a safe and convenient form of money backed by the goldsmith's promise to pay, allowing goldsmiths to advance loans with little risk of default. Thus the goldsmiths of London became the forerunners of banking by creating new money based on credit.</p>
+<p>The Bank of England originated the permanent issue of banknotes in 1695. The Royal Bank of Scotland established the first overdraft facility in 1728. By the beginning of the 19th century Lubbock's Bank had established a bankers' clearing house in London to allow multiple banks to clear transactions. The Rothschilds pioneered international finance on a large scale, financing the purchase of shares in the Suez canal for the British government in 1875.</p>
+<br>
+</div>`;
+
+var servicesContent = `<div class="servicesContent">
+<h1>Services</h1>
+<img src="images/signing.jpg"/>
+<p>Banks act as payment agents by conducting checking or current accounts for customers, paying cheques drawn by customers in the bank, and collecting cheques deposited to customers' current accounts. Banks also enable customer payments via other payment methods such as Automated Clearing House (ACH), Wire transfers or telegraphic transfer, EFTPOS, and automated teller machines (ATMs).
+</p><p>Banks borrow money by accepting funds deposited on current accounts, by accepting term deposits, and by issuing debt securities such as banknotes and bonds. Banks lend money by making advances to customers on current accounts, by making installment loans, and by investing in marketable debt securities and other forms of money lending.</p>
+<img src="images/vault.jpg"/>
+<p>Banks provide different payment services, and a bank account is considered indispensable by most businesses and individuals. Non-banks that provide payment services such as remittance companies are normally not considered as an adequate substitute for a bank account.</p>
+<p>Banks issue new money when they make loans. In contemporary banking systems, regulators set a minimum level of reserve funds that banks must hold against the deposit liabilities created by the funding of these loans, in order to ensure that the banks can meet demands for payment of such deposits. These reserves can be acquired through the acceptance of new deposits, sale of other assets, or borrowing from other banks including the central bank.</p>
+<br>
+<img src="images/bank.jpg"/>
+<h2>Channels</h2>
+<p>Banks offer many different channels to access their banking and other services:</p>
+<ul>
+<li>Branch, in-person banking in a retail location</li>
+<li>Automated teller machine banking adjacent to or remote from the bank</li>
+<li>Bank by mail: Most banks accept cheque deposits via mail and use mail to communicate to their customers</li>
+<li>Online banking over the Internet to perform multiple types of transactions</li>
+<li>Mobile banking is using one's mobile phone to conduct banking transactions</li>
+<li>Telephone banking allows customers to conduct transactions over the telephone with an automated attendant, or when requested, with a telephone operator</li>
+<li>Video banking performs banking transactions or professional banking consultations via a remote video and audio connection. Video banking can be performed via purpose built banking transaction machines (similar to an Automated teller machine) or via a video conference enabled bank branch clarification</li>
+<li>Relationship manager, mostly for private banking or business banking, who visits customers at their homes or businesses</li>
+<li>Direct Selling Agent, who works for the bank based on a contract, whose main job is to increase the customer base for the bank</li>
+</ul>
+<br>
+</div>`;
+
+var accountContent = `<div class="aboutContent">
+<img src="images/world.jpg" /><br>
+<h1>Account</h1>
+<p>Log-in or change your account.</p>
+<div class="form">
+<input type="text" placeholder="Username/e-mail..."><br><br>
+<input type="text" placeholder="Password..."><br><br>
+<img src="images/euro.jpg" /><br>
+<p>The follow two boxes are confidential! Be careful!!</p>
+<input type="text" placeholder="Account number..."><br><br>
+<input type="text" placeholder="Comment... (optional)" style="width: 35%">
+</div><br>
+<span>Log in</span><br><br>
+<p style="width:50%">Deposit and withdraw from your account. A deposit is money put into a bank account for safekeeping until you need it. A withdrawal is money that's taken out of your account.</p>
+<h2>Deposit</h2>
+<img src="images/two.jpg" /><br>
+<div class="form">
+<input type="number" placeholder="Amount..."><br>
+<h2>Withdraw</h2>
+<input type="number" placeholder="Amount..."><br>
+</form>
+<span>Deposit/withdraw</span>
+<br>
+<br>
+<br>
+</div>`;
+
+export {homeContent, historyContent, servicesContent, accountContent};
