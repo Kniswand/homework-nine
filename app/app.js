caf78fe5a4a2d7a28e114953d2f34a1a9d8c22df
Kniswand <122379489+Kniswand@users.noreply.github.com>
2023-04-17 15:44:35 -0400

    creating 'app' folder

---
+++ app/app.js
@@ -0,0 +1,19 @@
+import { homeContent, historyContent, servicesContent, accountContent } from "../model/model.js";
+
+function initListerners() {
+
+    $("nav a").on("click", function (e) {
+        let btnID = e.currentTarget.id;
+        let pageContentId = btnID + "Content";
+        changePageContent(pageContentId);
+    });
+}
+
+function changePageContent(pageContentIdVarName){
+    $("#app").html(eval(pageContentIdVarName));
+}
+
+$(document).ready(function () {
+    changePageContent('homeContent');
+    initListerners();
+});
